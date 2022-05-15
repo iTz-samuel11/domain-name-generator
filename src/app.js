e5c71b3a7ac72412761a9a420358cb4f7d19d10c
iTz-samuel11 <samuelandresfaraco@gmail.com>
2022-05-15 00:54:37 +0000

    finlizacion de proyecto

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronombres = ["the", "our"];
+  let adjs = ["great", "big"];
+  let nombres = ["jogger", "racoon"];
+  let extenciones = [".com ", ".net", ".io", "us"];
+
+  for (let pronombre of pronombres) {
+    for (let adj of adjs) {
+      for (let nombre of nombres) {
+        for (let extencion of extenciones) {
+          console.log(`${pronombre}${adj}${nombre}${extencion}`);
+        }
+      }
+    }
+  }
 };
